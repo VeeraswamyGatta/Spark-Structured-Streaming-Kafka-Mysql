2d50b854f60845e3a2db7ed9a8bc0a60ba3b05d7
Veeraswamy Gatta <veegatta@in.ibm.com>
2019-09-13 04:46:18 +0530

    Updating to Mysql sink

--- src/main/scala/com/spark/struct/SparkStructKafkaApp.scala
+++ src/main/scala/com/spark/struct/SparkStructKafkaApp.scala
@@ -1,5 +1,9 @@
 package com.spark.struct
 
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.{ProcessingTime, Trigger}
+import org.apache.spark.sql.types.{StructField, StructType, _}
+
 object SparkStructKafkaApp extends InitSpark {
 
   def main(args: Array[String]) = {
@@ -14,23 +18,67 @@ object SparkStructKafkaApp extends InitSpark {
       "key.deserializer" -> " org.apache.kafka.common.serialization.StringDeserializer",
       "value.deserializer" -> " org.apache.kafka.common.serialization.StringDeserializer",
       "group.id" -> "structured-kafka","startingOffsets"-> "earliest" , "maxOffsetsPerTrigger" -> "20" ,
-      "failOnDataLoss" -> "false","kafka.partition.assignment.strategy" -> "range")
+      "failOnDataLoss" -> "false")
+
+    val schema = StructType(Seq(
+      StructField("id", IntegerType, true),
+      StructField("firstName", StringType, true),
+      StructField("lastName", StringType, true),
+      StructField("dataUsage", LongType, true),
+      StructField("minutes", LongType, true),
+      StructField("billAmount", DoubleType, true)
+
+      /*StructField("id", StringType, true),
+      StructField("name", StringType, true),
+      StructField("gender", StringType, true),
+      StructField("cls", StringType, true),
+      StructField("seat", StringType, true),
+      StructField("club", StringType, true),
+      StructField("persona", StringType, true),
+      StructField("crush", StringType, true),
+      StructField("breastSize", StringType, true),
+      StructField("strength", StringType, true),
+      StructField("hairstyle", StringType, true),
+      StructField("color", StringType, true),
+      StructField("eyes", StringType, true),
+      StructField("eyeType", StringType, true),
+      StructField("stockings", StringType, true),
+      StructField("accessory", StringType, true),
+      StructField("scheduleTime", StringType, true),
+      StructField("scheduleDestination", StringType, true),
+      StructField("scheduleAction", StringType, true),
+      StructField("info", StringType, true)*/
+    ))
 
     val data = spark.
       readStream.
       format("kafka").
-      option("subscribe", "springcloud-stream-kafka-mesages").
+      option("subscribe", "bill_details").
       options(kafkaParams).
-      load().
-      selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").
-      as[(String, String)]
+      load().selectExpr("CAST(value AS STRING)")
 
-    data.printSchema()
+    val billDF = data.select(from_json(col("value").cast("string"), schema).as("data"))
+      .select("data.*")
 
-    data.writeStream.
-      outputMode("append").
-        format("console").
-          start().
-      awaitTermination()
+   /* billDF.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()*/
+
+    val url="jdbc:mysql://scdf-release-mysql:3306/dataflow"
+    val user ="root"
+    val pwd = "cwLAxh0JdN"
+
+    val writer = new JDBCSink(url,user, pwd)
+
+    billDF.writeStream
+      .foreach(writer)
+      .outputMode("update")
+      .trigger(Trigger.ProcessingTime("25 seconds"))
+      .start()
+      .awaitTermination()
+
+
   }
 }
